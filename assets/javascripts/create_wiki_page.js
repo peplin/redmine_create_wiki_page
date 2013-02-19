484c2efdc0a2a70f9c571775262ebf8bdb4258a7
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-18 22:05:25 -0500

    Only add "new" link if we're on the wiki.
    There isn't a hook for viewing the wiki, so we have to infer it based on the
    existing classes on the page.

--- assets/javascripts/create_wiki_page.js
+++ assets/javascripts/create_wiki_page.js
@@ -1,24 +1,26 @@
 $(document).ready(function($) {
-    var newLink = $("<a href=\"#\">New</a>");
-    $("#content .contextual").prepend(newLink);
-    $(newLink).click(function() {
-        var parent = $(this).parent();
-        var form = $("<form></form>");
-        form.append("<input type=\"text\">");
-        $(form).submit(function(e) {
-            e.preventDefault();
-            var name = $($(form).children()[0]).val();
-            var splitPath = window.location.pathname.split("/", 5);
-            var wikiPath = "/" + splitPath[1] + "/" + splitPath[2] + "/"
-                    + splitPath[3];
-            var parentName = splitPath[4];
-            var newPathname = "/" + name + "?parent=" + parentName;
-            window.location.assign(wikiPath + newPathname);
+    if($(".wiki-page").length > 0) {
+        var newLink = $("<a href=\"#\">New</a>");
+        $("#content .contextual").prepend(newLink);
+        $(newLink).click(function() {
+            var parent = $(this).parent();
+            var form = $("<form></form>");
+            form.append("<input type=\"text\">");
+            $(form).submit(function(e) {
+                e.preventDefault();
+                var name = $($(form).children()[0]).val();
+                var splitPath = window.location.pathname.split("/", 5);
+                var wikiPath = "/" + splitPath[1] + "/" + splitPath[2] + "/"
+                        + splitPath[3];
+                var parentName = splitPath[4];
+                var newPathname = "/" + name + "?parent=" + parentName;
+                window.location.assign(wikiPath + newPathname);
+                return false;
+            });
+
+            $(this).replaceWith(form);
+            form.children("input").focus();
             return false;
         });
-
-        $(this).replaceWith(form);
-        form.children("input").focus();
-        return false;
-    });
+    }
 });
