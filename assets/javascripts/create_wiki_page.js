fd916a9107794d4242e590cfa357fc988d94b188
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-02-18 21:51:36 -0500

    Add some fugly javascript to pre-fill parent wiki page.

--- assets/javascripts/create_wiki_page.js
+++ assets/javascripts/create_wiki_page.js
@@ -1,3 +1,24 @@
 $(document).ready(function($) {
-    $("#content .contextual").prepend("<a href=\"#\">New</a>");
+    var newLink = $("<a href=\"#\">New</a>");
+    $("#content .contextual").prepend(newLink);
+    $(newLink).click(function() {
+        var parent = $(this).parent();
+        var form = $("<form></form>");
+        form.append("<input type=\"text\">");
+        $(form).submit(function(e) {
+            e.preventDefault();
+            var name = $($(form).children()[0]).val();
+            var splitPath = window.location.pathname.split("/", 5);
+            var wikiPath = "/" + splitPath[1] + "/" + splitPath[2] + "/"
+                    + splitPath[3];
+            var parentName = splitPath[4];
+            var newPathname = "/" + name + "?parent=" + parentName;
+            window.location.assign(wikiPath + newPathname);
+            return false;
+        });
+
+        $(this).replaceWith(form);
+        form.children("input").focus();
+        return false;
+    });
 });
