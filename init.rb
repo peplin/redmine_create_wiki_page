5bd77e280841cb5622f39afc11e971daaf475cd1
ucho <ucho@fuchidori.com>
2013-04-07 21:18:40 +0900

    fixed: 500 error occurs at opening closed project.

--- init.rb
+++ init.rb
@@ -4,7 +4,6 @@ Redmine::Plugin.register :redmine_create_wiki_page do
   description 'This plugin adds a shortcut to create a new wiki page on each existing page'
   version '0.1'
   url 'http://github.com/peplin/redmine-create-wiki'
-  menu :project_menu, :new_wiki_page, '#', :caption => 'Create new wiki page'
 end
 
 require 'redmine_create_wiki_page/hooks.rb'
